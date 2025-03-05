0b994a672011c095261dc311dcf88fc2f1b6d8f1
Shreesh-125 <shreeshpolawar@gmail.com>
2025-03-05 23:20:23 +0530

    student controller updated

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -1,15 +1,4 @@
 const express = require('express');
-const { 
-    getAllProjects, 
-    getProjectById 
-} = require('../controllers/projectController');
-
 const router = express.Router();
-
-// GET by query
-router.get('/', getAllProjects);
-
-// GET by ID
-router.get('/:projectId', getProjectById);
-
+const userController = require('../controllers/projectController');
 module.exports = router;
--- src/routes/studentProfileRoutes.js
+++ src/routes/studentProfileRoutes.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/studentProfileController');
+const { 
+    getAllProjects, 
+    getProjectById 
+} = require('../controllers/studentProfileController');
+
+
+
+// GET by query
+router.get('/projects', getAllProjects);
+
+// GET by ID
+router.get('/projects/:projectId', getProjectById);
 module.exports = router;
