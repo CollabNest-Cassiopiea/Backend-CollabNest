1a93d210db76ccda411e3852d0796aa21f40bba7
codershivam45 <145230293+codershivam45@users.noreply.github.com>
2025-03-09 23:42:33 +0530

    Application Controllers and routes added

--- src/routes/applicationRoutes.js
+++ src/routes/applicationRoutes.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/applicationController');
+const { applyForProject ,getProjectApplications ,updateApplicationStatus } = require('../controllers/applicationController');
+
+
+router.post('/:projectId/applications' , applyForProject) ; // Student applies
+router.get('/:projectId/applications', getProjectApplications); // Mentor views applications
+router.put('/:projectId/applications/:appId', updateApplicationStatus); // Approve/Reject
+
 module.exports = router;
