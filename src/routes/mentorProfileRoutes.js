64574e58ca097fcb5de82ef1a2d4826304307466
Diptanshu-215 <diptanshusaurav215@gmail.com>
2025-03-20 16:14:15 +0530

    minor fixes

--- src/routes/mentorProfileRoutes.js
+++ src/routes/mentorProfileRoutes.js
@@ -1,4 +1,25 @@
+
+const {
+    createOrUpdateProfile,
+    getProfile,
+    getProjectsByFilters,
+    updateSkillRatings,
+    deleteProfile,
+} = require('../controllers/mentorProfileController');
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/mentorProfileController');
+
+// Create or Update MEntor Profile
+router.post('/:id/profile', createOrUpdateProfile);
+
+// Get Student Profile Details
+router.get('/:id/profile', getProfile);
+
+// Fetch Projects by Filters (e.g., skill, field)
+router.get('/projects', getProjectsByFilters);
+
+// Delete Student Profile
+router.delete('/:id/profile', deleteProfile);
+
 module.exports = router;
+
