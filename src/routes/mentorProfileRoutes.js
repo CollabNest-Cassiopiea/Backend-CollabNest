5df7aec56c18007d867a9a5cb19a1a144675c12d
Shreesh-125 <shreeshpolawar@gmail.com>
2025-03-20 17:10:14 +0530

    add mentor project controller and routes

--- src/routes/mentorProfileRoutes.js
+++ src/routes/mentorProfileRoutes.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const { authMiddleware, roleCheck } = require("../middlewares/authMiddleware");
+
 const router = express.Router();
 const userController = require('../controllers/mentorProfileController');
+
+
 module.exports = router;
+
+
+
+
+
+
